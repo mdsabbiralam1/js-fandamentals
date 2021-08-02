beaee4b288b698f0b5778a486f541dddad7af55e
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-02 23:49:12 +0600

    add all code

--- arry.js
+++ arry.js
@@ -1,1 +1,8 @@
-var
+var friendsAge = [45, 14, 20];
+var muriChanachurFee = [130, 23, 50, 5];
+var footballTeam = ['rakib', "shakib", "mamun"];
+console.log(footballTeam);
+console.log(muriChanachurFee);
+
+var footballTeam = footballTeam.length;
+console.log(footballTeam);
