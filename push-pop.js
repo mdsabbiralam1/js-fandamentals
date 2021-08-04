3af984119b03656b9176a6f7953d7e3ab88de2c6
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-04 21:52:17 +0600

    add

--- push-pop.js
+++ push-pop.js
@@ -1,2 +1,9 @@
-var lastbench = ['rakib, meraj, mamun, shohag, rifat']
-console.log(lastbench);
+var lastbench = ['rakib, meraj, mamun, shohag, rifat'];
+lastbench.push('hasib');
+lastbench.push('rifat');
+
+// console.log(lastbench);
+
+var friendsAge = [14, 23, 70];
+friendsAge.push(49, 59);
+console.log(friendsAge);
