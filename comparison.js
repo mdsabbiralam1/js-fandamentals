b4c23b0c459252e9d45efdc5381d70fd1d93ee93
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-04 22:57:47 +0600

    added comparison file

---
+++ comparison.js
@@ -0,0 +1,16 @@
+// console.log(5 > 6);
+// console.log(5 < 6);
+// console.log(4 == 7);
+// console.log(7 == 7);
+// console.log(5 != 7);
+// console.log(7 != 7);
+
+var myLove = 74;
+var yourLove = 85;
+console.log(myLove > yourLove);
+console.log(myLove == yourLove);
+console.log(myLove < yourLove);
+console.log(myLove != yourLove);
+
+console.log(goodStudent && goodPerson);
+console.log(isBanker || isGoodResult);
