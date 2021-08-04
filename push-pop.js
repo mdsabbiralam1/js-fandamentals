9f33d31928a54ca3bf75d7cfc0f57b75480ee41d
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-04 22:20:08 +0600

    added push-pop file

--- push-pop.js
+++ push-pop.js
@@ -1,9 +1,13 @@
+// add Elements to array
 var lastbench = ['rakib, meraj, mamun, shohag, rifat'];
 lastbench.push('hasib');
 lastbench.push('rifat');
-
 // console.log(lastbench);
 
-var friendsAge = [14, 23, 70];
-friendsAge.push(49, 59);
+
+// ramove an element from an array
+var friendsAge = [14, 23, 70, 54, 89];
+friendsAge.pop();
+friendsAge.shift();
+friendsAge.unshift(77);
 console.log(friendsAge);
