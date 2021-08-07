6355f9a16eb490fb4b3e96eda762b4803ed727e1
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-08 00:44:56 +0600

    add nested file

---
+++ nested-condition.js
@@ -0,0 +1,24 @@
+var danishPrice = 50;
+var butterBreadPrice = 25;
+var toastBuscitPrice = 10;
+var myBudget = 9;
+
+// if (myBudget > danishPrice) {
+//     console.log('danis kheye danish desh e jabo');
+// }
+// else if (myBudget > butterBreadPrice) {
+//     console.log('butter bread kheye butterfly er moto akashe urbo..')
+// }
+// else if (myBudget > toastBuscitPrice) {
+//     console.log('toast buscite diye cha khamo');
+// }
+// else {
+//     console.log('batasa diye cha khamo');
+// }
+
+if (danishPrice < myBudget) {
+    console.log('ami danish khamo');
+}
+else if (myBudget > toastBuscitPrice) {
+    console.log('ami ruti khamo')
+}
