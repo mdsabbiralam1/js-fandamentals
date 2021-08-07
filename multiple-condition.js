3445a0af5b3de41c041c96b034f7bdd891a36547
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-07 23:55:36 +0600

    add done

---
+++ multiple-condition.js
@@ -0,0 +1,22 @@
+var gotjob = true;
+var moneySaved = 250000;
+var hasFlat = false;
+
+if (gotjob == true && moneySaved > 200000 || hasFlat == true) {
+    console.log('cholo amra biye kore feli !!!');
+}
+else {
+    console.log('tor kopale biye nei..ja sor ekhan theke');
+}
+
+var chanacore = true;
+var muri = true;
+var dal = false;
+var ada = false;
+
+if ((chanacore == true && dal == true) || ada == true) {
+    console.log('jal muri khamo dada.');
+}
+else {
+    console.log('tor kopale jal muri nai');
+}
