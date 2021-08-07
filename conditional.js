71c877cf752188557cbeaf74417e71390eeb16b4
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-07 22:49:08 +0600

    add code

--- conditional.js
+++ conditional.js
@@ -1,2 +1,32 @@
 // jodi food ready hoy taile ami khamo
 // jodi food ready na hoy taile ami khamo na
+// iff food ready  i will eat 
+// iff condition
+
+
+var isFoodReady = false;
+if (isFoodReady == false) {
+    console.log('mama! i will eat');
+}
+// jodi taka thake taile kinbo boi
+var book = true;
+if (book = true) {
+    console.log('mama boi kinmo')
+}
+
+// jodi iphon er dam amar budget er theke besi hoy tobe ami kinte parbo.
+var iphonePrice = 70000;
+var budget = 8950;
+if (iphonePrice > budget) {
+    console.log('mama aamar mi best');
+}
+
+
+var headPhonePrice = 3500;
+var cashMoney = 3400;
+if delevary(headPhonePrice > cashMoney) {
+    console.log('Product Delevary Successfull');
+}
+if delevary(headPhonePrice < cashMoney) {
+    console.log('Product Delevary Faild');
+}
